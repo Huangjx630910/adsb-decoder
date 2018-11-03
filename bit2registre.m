5dbb1f292a72e04b45f0a5b636bcce2f03f83857
Théo LEPINE <theo.lepine@gmail.com>
2018-11-03 09:22:52 +0100

    Début longitude. Petites subtilités autrichiennes à corriger!

--- bit2registre.m
+++ bit2registre.m
@@ -25,20 +25,29 @@ if error==0 % Si pas d'erreur dans le CRC
             registre.nom = char(nom);
         elseif ((type >= 5) && (type <= 8)) % Localisation
             %%> TIME FLAG UTC
-            registre.timeFlag = int2str(data(21));
+            registre.timeFlag = int2str(data(53));
             %%> CPR FLAG
-            registre.cprFlag = int2str(data(22));
+            registre.cprFlag = int2str(data(54));
             %%> LATITUDE
             lat_ref = 44.806744; % prise à l'ENSEIRB depuis GMaps
             LAT = bi2de(data(55:71)', 'left-msb');
             Dlat = 360 / (4*15 - registre.cprFlag); % 15=Nz
             j = floor(lat_ref/Dlat) + floor(1/2 ... 
                 + (lat_ref-Dlat*floor(lat_ref/Dlat))/Dlat ...
-                - LAT/(2^17)); % 17=Nb
-            registre.latitude = Dlat*(j+ LAT/(2^17));
+                - LAT/(2^17));
+            registre.latitude = Dlat*(j+ LAT/2^17);
             %%> LONGITUTDE
             long_ref = -0.606100; % prise à l'ENSEIRB depuis GMaps
-            LON =bi2de(data(72:88)', 'left-msb');
+            LON = bi2de(data(72:88)', 'left-msb');
+            if((cprNL(registre.latitude) - registre.cprFlag)>0) % calcul de Dlon
+                Dlon = 360/(cprNL(registre.latitude) - registre.cprFlag);
+            else
+                Dlon = 360;
+            end
+            m = floor(long_ref/Dlon) ...
+                + floor(1/2 + (long_ref - Dlon*floor(long_ref/Dlon))/Dlon ...
+                - (LON/2^17) );
+            registre.longitude = Dlon*(m + LON/2^17);
         end    
     end % END if(type == 17)
 end % END if pas d'erreur dans le CRC

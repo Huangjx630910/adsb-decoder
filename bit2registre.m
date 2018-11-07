bc63bf1aebfd4838dd3ad1a1d182004aac5388a7
Théo LEPINE <theo.lepine@gmail.com>
2018-11-07 10:17:27 +0100

    Trajectoire et décodage fonctionnels

--- bit2registre.m
+++ bit2registre.m
@@ -6,11 +6,11 @@ detecteur = crc.detector(crc_poly);
 
 if error==0 % Si pas d'erreur dans le CRC
     %%> FORMAT
-    registre.format = int2str(bi2de(data(1:5)'));
+    registre.format = int2str(bi2de(data(1:5)', 'left-msb'));
     %%> ADRESSE
     registre.adresse = dec2hex(bin2dec(int2str(data(9:32)')));
     %%> TYPE
-    type = bi2de(data(33:37)');
+    type = bi2de(data(33:37)', 'left-msb');
     registre.type = int2str(type);
     
     if(registre.format == '17') % on ne considère que ce type de messages ADS-B
@@ -23,31 +23,39 @@ if error==0 % Si pas d'erreur dans le CRC
             lettres = nom<27;
             nom(lettres) = nom(lettres)+64; % décalage par rapport à la table ASCII pour les lettres
             registre.nom = char(nom);
-        elseif ((type >= 5) && (type <= 8)) % Localisation
+        elseif ((type >= 9) && (type <= 18)) % Localisation
             %%> TIME FLAG UTC
             registre.timeFlag = int2str(data(53));
             %%> CPR FLAG
-            registre.cprFlag = int2str(data(54));
+            i = data(54);
+            registre.cprFlag = int2str(i);
+            %%> ALTITUDE
+            altitude = 25 * bi2de([data(41:47)' data(49:52)'], 'left-msb') - 1000;
+            registre.altitude = int2str(altitude);
             %%> LATITUDE
-            lat_ref = 44.806744; % prise à l'ENSEIRB depuis GMaps
+            lat_ref = 44.806575; % prise à l'ENSEIRB depuis GMaps
             LAT = bi2de(data(55:71)', 'left-msb');
-            Dlat = 360 / (4*15 - registre.cprFlag); % 15=Nz
+            Dlat = 360 / (4*15 - i); % 15=Nz
             j = floor(lat_ref/Dlat) + floor(1/2 ... 
                 + (lat_ref-Dlat*floor(lat_ref/Dlat))/Dlat ...
                 - LAT/(2^17));
-            registre.latitude = Dlat*(j+ LAT/2^17);
+            latitude = Dlat*(j+ LAT/2^17);
+            registre.latitude = int2str(latitude);
             %%> LONGITUTDE
-            long_ref = -0.606100; % prise à l'ENSEIRB depuis GMaps
+            long_ref = -0.605654; % prise à l'ENSEIRB depuis GMaps
             LON = bi2de(data(72:88)', 'left-msb');
-            if((cprNL(registre.latitude) - registre.cprFlag)>0) % calcul de Dlon
-                Dlon = 360/(cprNL(registre.latitude) - registre.cprFlag);
+            if((cprNL(latitude) - i)>0) % calcul de Dlon
+                Dlon = 360/(cprNL(latitude) - i);
             else
                 Dlon = 360;
             end
             m = floor(long_ref/Dlon) ...
                 + floor(1/2 + (long_ref - Dlon*floor(long_ref/Dlon))/Dlon ...
                 - (LON/2^17) );
-            registre.longitude = Dlon*(m + LON/2^17);
+            longitude = Dlon*(m + LON/2^17);
+            registre.longitude = int2str(longitude);
+            %%> TRAJECTOIRE
+            registre.trajectoire = [registre.trajectoire ; [latitude longitude]];
         end    
     end % END if(type == 17)
 end % END if pas d'erreur dans le CRC

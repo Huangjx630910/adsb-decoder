7ac6614cca86d6308c3f3ad31b43b5014a5ae08d
Théo LEPINE <theo.lepine@gmail.com>
2018-11-28 17:57:32 +0100

    Rendu final

--- ADBS_part1_5_LEPINE_KIEWSKY.m
+++ ADBS_part1_5_LEPINE_KIEWSKY.m
@@ -3,19 +3,21 @@ clear; close all; clc; dbstop if error;
 %% Déclaration des variables
 N = 88; % Nombre de bits d'information
 Ts = 1e-6;
-Tp=8*Ts;
+Tp = 8*Ts;
 fe = 20e6;
 Te = 1/fe;
-Fse = Ts/Te; % Ts/Te dans même unités
+Fse = Ts/Te; % Ts/Te dans même unité
 
 %% Encodage CRC
 bk = randi([0,1], N, 1);
 crc_poly = [1 1 1 1 1 1 1 1 1 1 1 1 1 0 1 0 0 0 0 0 0 1 0 0 1];
 generateur = crc.generator(crc_poly);
 encoded = generate(generateur, bk);
-Ak = (-2*encoded)+1;
 
 %% Signal sl
+% Formation du message binaire de symboles Ak
+Ak = (-2*encoded)+1;
+
 % Formation du signal modulé PPM sl(t)
 p=[-0.5*ones(1,10) 0.5*ones(1,10)];
 upsampled = upsample(encoded, Fse);
@@ -25,6 +27,7 @@ sl = sl(1:end-retard_introduit);
 sl=sl';
 
 %% Simulation du signal recu (utile pour vérification de delta_t)
+
 % Ajout du préambule 
 one=ones(1,10);
 zero=zeros(1,10);
@@ -36,11 +39,11 @@ delta_t_avion = floor(100*rand(1))
 sl_retard = [randi([0 1],1,delta_t_avion) sl];
 
 %Génération du bruit
-sigma2=1;
-nl = randn(1,length(sl_retard))*sqrt(sigma2) + 1j*randn(1,length(sl_retard))*sqrt(sigma2); % BBGC complexe au rythme Te (car sl est complexe, le bruit doit s'appliquer aux 2 parties)
+sigma2=1; % A verif
+nl=0; % sans bruit pour le test
 
 % Ajout du bruit
-yl_retard = sl_retard+nl;
+yl_retard = sl_retard+nl; 
 
 
 %% Synchronisation 
@@ -56,11 +59,10 @@ rl=sl_retard+zl;
 est_delta_t = synchronisation(rl, sp, liste_delta_t, Tp/Ts, Te, Fse) 
 
 % Synchronisation en temps du signal recu sans le préambule
-yl=yl_retard(1,est_delta_t+length(sp):length(yl_retard));
+yl=yl_retard(1,est_delta_t+length(sp)+1:end); 
 yl=yl';
 
 %% Signal rl et rm
-
 % Filtre adapté - Convolution pour obtenir rl(t)
 rl=conv(yl, fliplr(p))*Te;
 
@@ -68,8 +70,13 @@ rl=conv(yl, fliplr(p))*Te;
 rm=rl(length(p):Fse:length(Ak)*Fse)/Te;
 
 %% Décision
-decoded_est = rm<0;
+decoded_est = rm>0;
 
 %% Décodage CRC
+
 detecteur = crc.detector(crc_poly);
 [decoded error] = detect(detecteur, decoded_est);
+
+
+%% Restitution des résultats
+error
